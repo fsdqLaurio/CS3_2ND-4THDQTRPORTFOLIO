15e081d131eb0b25ff9ca9bbe6eb0d5f4e437789
fsdqLaurio <b2029fsdqlaurio@pshs.edu.ph>
2026-04-08 04:42:36 +0800

    3ge debugging

--- public/q4/3rd_graded/script.js
+++ public/q4/3rd_graded/script.js
@@ -1,12 +1,21 @@
-function plotPoint() {
+function plotPoint(x0, y0, x, y) {
+
     console.log(x0, y0, x, y) // checks the arguments passed to this function
+   
+    let in1 = document.getElementById('in1'); 
     in1.innerHTML = x0 + " " + y0 + " " + x + " " + y;
+    
     var point = document.createElement('div');  
     point.className = 'point';
     /* control where to place the div on the screen using left and bottom of position:absolute */
-    point.styleleft = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
-    point.stylebottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
-    document.getElementById('coordinatePlane').appendChild(point);
+    
+    point.style.left = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
+    point.style.bottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
+    
+    let coordinatePlane = document.getElementById('coordinatePlane');
+    coordinatePlane.appendChild(point);
+
+    let out1 = document.getElementById('out1');
 
     if (x > x0 && y > y0){
       out1.innerHTML = "NE"
@@ -23,4 +32,5 @@ function plotPoint() {
     else{
       out1.innerHTML = "divisa"
     }
+
   }
